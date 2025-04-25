f4fb73be9cf83e32927cfbb4a312e18d68ac4d64
rodolfosouzamoraes <rodolfomsouza@gmail.com>
2025-04-25 08:41:20 -0300

    Niveis Inimigos

--- Assets/InstanciarObjeto.cs
+++ Assets/InstanciarObjeto.cs
@@ -5,11 +5,14 @@ public class InstanciarObjeto : MonoBehaviour
     public GameObject objeto; //Objeto a ser instanciado
     public float tempoSurgimento; //Tempo para surgir cada objeto novo
     protected float tempoEspera; //Tempo para permitir surgir cada objeto novo
+    public bool eInimigo; //Define se o objeto é um inimigo
+    private CanvasGameMng canvasGameMng;//Referencia do canvas game mng
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Referenciar o CanvasGameMng
+        canvasGameMng = FindFirstObjectByType<CanvasGameMng>();
     }
 
     // Update is called once per frame
@@ -26,6 +29,13 @@ public class InstanciarObjeto : MonoBehaviour
 
             //Instanciar o objeto
             GameObject novoObjeto = Instantiate(objeto);
+
+            //Verificar se o objeto é um inimigo
+            if (eInimigo == true) {
+                //Definir o nivel do inimigo
+                int nivelJogo = canvasGameMng.nivelJogo;
+                novoObjeto.GetComponent<DanoAoInimigo>().DefinirNivelInimigo(nivelJogo);
+            }
 
             //Posicionar o objeto na coordenada X e Y
             novoObjeto.transform.position = new Vector3(posicaoX, 7.5f, 0);
